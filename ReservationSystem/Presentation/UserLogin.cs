a976f846d8ddd45555418baff4db48a9b5884a93
Kaylan de Groot <degrootkaylan@gmail.com>
2024-03-18 17:52:58 +0100

    Refactored code & added comments

--- ReservationSystem/Presentation/UserLogin.cs
+++ ReservationSystem/Presentation/UserLogin.cs
@@ -5,23 +5,30 @@ static class UserLogin
 
     public static void Start()
     {
-        Console.WriteLine("Welcome to the login page");
+        Console.Clear();
+        Console.WriteLine("Welcome to the login page\n");
         Console.WriteLine("Please enter your email address");
         string email = Console.ReadLine();
         Console.WriteLine("Please enter your password");
         string password = Console.ReadLine();
+
+        // Check if account exists
         AccountModel acc = accountsLogic.CheckLogin(email, password);
+
+        // Show message if user is logged in successfully or not
+        // And return to menu
         if (acc != null)
         {
+            Console.Clear();
             Console.WriteLine("Welcome back " + acc.FullName);
-            Console.WriteLine("Your email number is " + acc.EmailAddress);
 
-            // Write some code to go back to the menu
-            Menu.Start();
         }
         else
         {
+            Console.Clear();
             Console.WriteLine("No account found with that email and password");
         }
+
+        Menu.Start();
     }
 }
