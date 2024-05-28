d8c7eda1f84b2d9ff637132fcb9611dad50ae9e4
Kaylan de Groot <degrootkaylan@gmail.com>
2024-05-28 11:16:23 +0200

    Added methods the filter reservations&accounts

--- ReservationSystem/Logic/StatisticLogic.cs
+++ ReservationSystem/Logic/StatisticLogic.cs
@@ -1,10 +1,112 @@
+using System.Globalization;
+
 public class StatisticLogic
 {
     private List<ReservationModel> _reservations;
     private List<AccountModel> _accounts;
 
-    public void GetStatistics()
-    {
 
+    public StatisticLogic()
+    {
+        _reservations = ReservationAccess.LoadAll();
+        _accounts = AccountsAccess.LoadAll();
+    }
+
+    public (double, int, int) GetStatistics()
+    {
+        double totalProfit = _reservations.Sum(x => x.TotalCost);
+        int totalReservations = _reservations.Count;
+        int totalNewUsers = _accounts.Count(x => x.Role == "user");
+
+        return new(totalProfit, totalReservations, totalNewUsers);
+    }
+
+    public (double, int, int) GetStatisticsMonthly(DateTime date)
+    {
+        List<ReservationModel> filteredResv = _reservations
+        .Where(x =>
+        {
+            DateTime resvDate = DateTime.ParseExact(x.ReservationDate, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            return resvDate.Month == date.Month && resvDate.Year == date.Year;
+        })
+        .ToList();
+
+        List<AccountModel> filteredAcc = _accounts
+        .Where(x =>
+        {
+            DateTime accDate = DateTime.ParseExact(x.DateCreated, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            return accDate.Month == date.Month && accDate.Year == date.Year;
+        })
+        .ToList();
+
+        double totalProfit = filteredResv.Sum(x => x.TotalCost);
+        int totalReservations = filteredResv.Count;
+        int totalNewUsers = filteredAcc.Count(x => x.Role == "user");
+
+        return new(totalProfit, totalReservations, totalNewUsers);
+    }
+
+    public (double, int, int) GetStatisticsWeekly(DateTime startDate, DateTime endDate)
+    {
+        List<ReservationModel> filteredResv = _reservations
+        .Where(x =>
+        {
+            DateTime resvDate = DateTime.ParseExact(x.ReservationDate, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            return resvDate >= startDate && resvDate <= endDate;
+        })
+        .ToList();
+
+        List<AccountModel> filteredAcc = _accounts
+        .Where(x =>
+        {
+            DateTime accDate = DateTime.ParseExact(x.DateCreated, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            return accDate >= startDate && accDate <= endDate;
+        })
+        .ToList();
+
+        double totalProfit = filteredResv.Sum(x => x.TotalCost);
+        int totalReservations = filteredResv.Count;
+        int totalNewUsers = filteredAcc.Count(x => x.Role == "user");
+
+        return new(totalProfit, totalReservations, totalNewUsers);
+    }
+
+    public (double, int, int) GetStatisticsDaily(DateTime date)
+    {
+        List<ReservationModel> filteredResv = _reservations
+        .Where(x =>
+        {
+            DateTime resvDate = DateTime.ParseExact(x.ReservationDate, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            return resvDate.Month == date.Month && resvDate.Year == date.Year && resvDate.Day == date.Day;
+        })
+        .ToList();
+
+        List<AccountModel> filteredAcc = _accounts
+        .Where(x =>
+        {
+            DateTime accDate = DateTime.ParseExact(x.DateCreated, "dd-MM-yyyy HH:mm", CultureInfo.InvariantCulture);
+            return accDate.Month == date.Month && accDate.Year == date.Year && accDate.Day == date.Day;
+        })
+        .ToList();
+
+        double totalProfit = filteredResv.Sum(x => x.TotalCost);
+        int totalReservations = filteredResv.Count;
+        int totalNewUsers = filteredAcc.Count(x => x.Role == "user");
+
+        return new(totalProfit, totalReservations, totalNewUsers);
+    }
+
+
+
+    public (DateTime FirstDayOfWeek, DateTime LastDayOfWeek) GetWeekBoundaries(DateTime date)
+    {
+        CultureInfo cultureInfo = CultureInfo.CurrentCulture;
+        DayOfWeek firstDayOfWeek = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+        int diff = (7 + (date.DayOfWeek - firstDayOfWeek)) % 7;
+
+        DateTime firstDay = date.AddDays(-1 * diff).Date;
+        DateTime lastDay = firstDay.AddDays(6).Date;
+
+        return (firstDay, lastDay);
     }
 }
--- ReservationSystem/Presentation/AdminStatistics.cs
+++ ReservationSystem/Presentation/AdminStatistics.cs
@@ -1,18 +1,24 @@
+using System.Globalization;
 public static class AdminStatistics
 {
-    private static DateTime _today = DateTime.Now;
+    private static DateTime _date;
     private static List<String> _viewOptions = new List<string> { "All time", "Monthly", "Weekly", "Daily" };
     private static int _selectedOption;
-    private static DateTime _searchDate;
-
+    private static string _searchDate;
+    private static StatisticLogic _statisticLogic = new StatisticLogic();
     public static void Start()
     {
+        _date = DateTime.Now;
         _selectedOption = 0;
         bool stop = false;
 
         while (!stop)
         {
             Console.Clear();
+            Console.WriteLine("Press the up/down arrow to navigate the menu");
+            Console.WriteLine("Press the left/right arrow to change date");
+            Console.WriteLine("Press Backspace to return the main menu");
+
             Statistics();
             ViewOptions();
 
@@ -21,13 +27,49 @@ public static class AdminStatistics
 
             if (pressedKey == ConsoleKey.DownArrow)
             {
+                _date = DateTime.Now;
                 _selectedOption++;
                 if (_selectedOption == _viewOptions.Count) _selectedOption = 0;
             }
             else if (pressedKey == ConsoleKey.UpArrow)
             {
+                _date = DateTime.Now;
                 _selectedOption--;
                 if (_selectedOption == -1) _selectedOption = _viewOptions.Count - 1;
+            }
+            else if (pressedKey == ConsoleKey.LeftArrow)
+            {
+                switch (_selectedOption)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        _date = _date.AddMonths(-1);
+                        break;
+                    case 2:
+                        _date = _date.AddDays(-7);
+                        break;
+                    case 3:
+                        _date = _date.AddDays(-1);
+                        break;
+                }
+            }
+            else if (pressedKey == ConsoleKey.RightArrow)
+            {
+                switch (_selectedOption)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        _date = _date.AddMonths(1);
+                        break;
+                    case 2:
+                        _date = _date.AddDays(7);
+                        break;
+                    case 3:
+                        _date = _date.AddDays(1);
+                        break;
+                }
             }
             else if (pressedKey == ConsoleKey.Backspace)
             {
@@ -56,8 +98,32 @@ public static class AdminStatistics
 
     public static void Statistics()
     {
-        string stats = $"║ Profit: €0 ║ Reservations: 100 ║ New Users: 0 ║";
+        (double, int, int) data = _statisticLogic.GetStatistics();
+
+        switch (_selectedOption)
+        {
+            case 0:
+                _searchDate = "All time";
+                break;
+            case 1:
+                data = _statisticLogic.GetStatisticsMonthly(_date);
+                _searchDate = _date.ToString("Y", CultureInfo.CreateSpecificCulture("en-US"));
+                break;
+            case 2:
+                (DateTime, DateTime) week = _statisticLogic.GetWeekBoundaries(_date);
+                data = _statisticLogic.GetStatisticsWeekly(week.Item1, week.Item2);
+                _searchDate = week.Item1.ToString("D", CultureInfo.CreateSpecificCulture("en-US")) + " / " + week.Item2.ToString("D", CultureInfo.CreateSpecificCulture("en-US"));
+                break;
+            case 3:
+                data = _statisticLogic.GetStatisticsDaily(_date);
+                _searchDate = _date.ToString("D", CultureInfo.CreateSpecificCulture("en-US"));
+                break;
+        }
+
+        string stats = $"║ Profit: €{data.Item1} ║ Reservations: {data.Item2} ║ New Users: {data.Item3} ║";
         string border = new string('═', stats.Length - 2);
+
+        Console.WriteLine($"\n[{_searchDate}]\n");
         Console.WriteLine("╔" + border + "╗");
         Console.WriteLine(stats);
         Console.WriteLine("╚" + border + "╝");
