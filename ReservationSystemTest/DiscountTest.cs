39bc838cc2946e79673397dc65ca8ab1cf518a93
Kaylan de Groot <degrootkaylan@gmail.com>
2024-06-14 21:28:12 +0200

    Fixed discount code unit test

--- ReservationSystemTest/DiscountTest.cs
+++ ReservationSystemTest/DiscountTest.cs
@@ -18,7 +18,15 @@ public class DiscountTest
         logic.CreateDiscount("TEST2", 50, "01-08-2024 10:00", "30-08-2024 10:00");
         DiscountModel discount = logic.GetDiscount("TEST2");
         double price = logic.GetDiscountPrice(discount, 75.0);
-
         Assert.AreEqual(price, 37.5);
+
+        price = logic.GetDiscountPrice(discount, 50);
+        Assert.AreEqual(price, 25.0);
+
+        price = logic.GetDiscountPrice(discount, 43);
+        Assert.AreEqual(price, 21.5);
+
+        price = logic.GetDiscountPrice(discount, 1548);
+        Assert.AreEqual(price, 774.0);
     }
 }
